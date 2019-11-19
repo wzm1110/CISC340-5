1cce1898da997686d6ac9f279db325646fc73e8e
wzm1110 <37986358+wzm1110@users.noreply.github.com>
2019-11-18 19:47:20 -0500

    Update read_serial.py

--- read_serial.py
+++ read_serial.py
@@ -1,7 +1,9 @@
+
 #!/usr/bin/env python
 import time
 import serial
 import smtplib
+import sys
 '''
 This is the python script for cisc340 project, which includes the function of minicom to show
 the reading of data from serial port, and can also send notification to users when certain 
@@ -30,40 +32,61 @@ ser = serial.Serial(
  timeout=1
 )
 
-fire_detected = False
-breach_detected = False
 
-while 1:
- reading = 0
- magnet = 0
- x=ser.readline()
- if "button" in x:
-     if x[-2] != '0':
-	breach_detected = False
-	fire_detected = False
- elif "jbin" in x:
-     if x[14:16] == "FD" and breach_detected == False:
-	print "breach"
-        s = smtplib.SMTP('smtp.gmail.com', 587)
-        s.ehlo()
-        s.starttls()
-        s.ehlo()
-        s.login(smtpUser, smtpPass)
-        s.sendmail(fromAdd, toAdd, header + '\n' + body_breach)
-        breach_detected = True
-	s.quit()
- elif "VAUX14" in x :
-     if len(x) == 19:
-         reading = x[15:17]
-     elif len(x) == 20:
-         reading =  x[15:18]
-     if int(reading) > 100 and fire_detected == False:
-         print "fire"
-         s = smtplib.SMTP('smtp.gmail.com', 587)
-         s.ehlo()
-         s.starttls()
-         s.ehlo()
-         s.login(smtpUser, smtpPass)
-         s.sendmail(fromAdd, toAdd, header + '\n' + body_fire)
-         fire_detected = True
-	 s.quit()
+def system(arm,status):
+    if arm:
+        return check(status)
+    else:
+        return [False, False] 
+
+def check(status):
+     fire_detected = status[0]
+     breach_detected = status[1]
+     reading = 0      
+     while not breach_detected and not fire_detected:
+     	x=ser.readline()
+     	#print x
+	if "button" in x:
+            if x[-2] != '0':
+                breach_detected = False
+                fire_detected = False
+		
+     	elif "jbin" in x and not breach_detected:
+            if x[14:16] == "FD":
+                print "breach"
+                print breach_detected, fire_detected
+                s = smtplib.SMTP('smtp.gmail.com', 587)
+                s.ehlo()
+                s.starttls()
+                s.ehlo()
+                s.login(smtpUser, smtpPass)
+                s.sendmail(fromAdd, toAdd, header + '\n' + body_breach)
+                print breach_detected, fire_detected
+		s.quit()
+		breach_detected = True
+
+        elif "VAUX14" in x and not fire_detected:
+             if len(x) == 19:
+                 reading = x[15:17]
+             elif len(x) == 20:
+                 reading =  x[15:18]
+             if int(reading) > 100:
+                 print "fire"
+                 print fire_detected, breach_detected
+		 s = smtplib.SMTP('smtp.gmail.com', 587)
+                 s.ehlo()
+             	 s.starttls()
+             	 s.ehlo()
+             	 s.login(smtpUser, smtpPass)
+             	 s.sendmail(fromAdd, toAdd, header + '\n' + body_fire)
+             	 print fire_detected, breach_detected
+		 s.quit()
+		 fire_detected = True
+        #else:
+            #fire_detected, breach_detected = False, False
+     return [fire_detected, breach_detected]
+def isFire():
+    return fire_detected
+
+def isBreached():
+    return breach_detected
